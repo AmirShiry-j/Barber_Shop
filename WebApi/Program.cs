1ca3d2d4cd929e7516d53beb2ffba4c88e9069e3
AmirShiry06@gmail.com <AmirShiry06@gmail.com>
2022-11-03 01:17:47 +0330

    Swagger settings enabled

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,9 +1,11 @@
-using Application.Interfaces.Contexts;
+﻿using Application.Interfaces.Contexts;
 using Domain.Users;
 using Infrastructure.EmailService;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.OpenApi.Models;
 using Persistence.Contexts;
 using WebApi.Tools.PersianError;
 
@@ -55,6 +57,42 @@ builder.Services.Configure<IdentityOptions>(options =>
     options.SignIn.RequireConfirmedAccount = true;
 });
 
+//Config Swagger
+builder.Services.AddSwaggerGen(c =>
+{
+    //برای نمایش استرینگی Enum ها
+    //c.DescribeAllEnumsAsStrings();
+
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Barber Shop", Version = "v1" });
+
+    //برای نمایش Description کنترلر ها
+    //c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "WebApi.xml"), true);
+
+    //For configure Authentication in swaager Ui
+    var security = new OpenApiSecurityScheme
+    {
+        Name = "JWT Auth",
+        Description = "توکن را وارد کنید",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        Reference = new OpenApiReference
+        {
+            Id = JwtBearerDefaults.AuthenticationScheme,
+            Type = ReferenceType.SecurityScheme
+        }
+    };
+    c.AddSecurityDefinition(security.Reference.Id, security);
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    { security , new string[]{ } }
+                });
+});
+
+
+
+
 ////Services of DB
 //Db service
 builder.Services.AddScoped<IDataBaseContext, DataBaseContext>();
@@ -68,6 +106,17 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+//Swagger config
+app.UseSwagger(c =>
+{
+    c.SerializeAsV2 = false;
+});
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApi v1");
+    c.RoutePrefix = string.Empty;
+});
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -77,6 +126,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
